277ce5ea4a977f72d5334deed08a2ea80a5ec0c3
文艺清 <123439172+wenyiqing123@users.noreply.github.com>
2024-10-05 08:01:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const config = {
   accounts: [
     {
       username: "19553766906", //用户名
-      password: "yq0040809", //密码
+      password: "yq20040809", //密码
       openId: "", //微信小程序抓包openid(可选)
       unionId: "", //微信小程序抓包unionId(可选)
       sign: true, //是否自动签到
       reSign: false, //是否重新签到
-      location:"",
+      location:"117.12027499999999,37.192128",
       // location: "120.48587700000007,36.15023000000001", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
       // signImagePath: "./images/1.jpeg", //签到图片
       // needReport: false, //是否自动填写周报
